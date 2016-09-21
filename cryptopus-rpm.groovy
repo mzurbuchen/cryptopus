d60ba53ab385263491245259ea46483b69cde2a5
Sophia Katzy <katzy@puzzle.ch>
2016-09-21 09:18:12 +0200

    upload cryptopus rpm to pulp

--- cryptopus-rpm.groovy
+++ cryptopus-rpm.groovy
@@ -3,13 +3,13 @@ node('buildnode') {
   def fpm       = "${fpmHome}/bin/fpm"
   def pulp_repo = 'puzzle-autobuild-cryptopus2'
   def deploy_path   = "/var/www/vhosts"
-  def current_build = "cryptopus-2.0-${env.BUILD_NUMBER}.x86_64.rpm"
+  def current_build = "cryptopus-2.0-${BUILD_NUMBER}.x86_64.rpm"
 
   git('https://github.com/puzzle/cryptopus')
   sh """
     set -x
-    ${fpmHome}/bin/fpm -s dir -t rpm -n cryptopus -a x86_64 --iteration ${env.BUILD_NUMBER} -m puzzle-itc --prefix ${deploy_path} .
-    /usr/local/bin/upload_rpm_to_pulp.sh ${pulp_repo} *.rpm
+    ${fpmHome}/bin/fpm -s dir -t rpm -n ${current_build} -a x86_64 -m puzzle-itc --prefix ${deploy_path} .
+    /usr/local/bin/upload_rpm_to_pulp.sh ${pulp_repo} ${current_build}
     #rm -rf ./*.rpm
   """
   archive"${current_build}"
