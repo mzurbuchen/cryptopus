5ed0a7d59a2280367f7522a8d346478140fe0a59
Sophia Katzy <katzy@puzzle.ch>
2016-09-21 09:39:59 +0200

    upload cryptopus rpm to pulp

--- cryptopus-rpm.groovy
+++ cryptopus-rpm.groovy
@@ -8,7 +8,7 @@ node('buildnode') {
   sh """
     set -x
     rm -rf ./*.rpm
-    ${fpmHome}/bin/fpm -s dir -t rpm -n cryptopus -m puzzle-itc -v 2.0.${env.BUILD_NUMBER} --prefix ${deploy_path} .
+    ${fpmHome}/bin/fpm -s dir -t rpm -n cryptopus -m puzzle-itc -v 2.0.${env.BUILD_NUMBER} --after-install ./postinstall.sh --prefix ${deploy_path} .
     /usr/local/bin/upload_rpm_to_pulp.sh ${pulp_repo} *.rpm
   """
 }
