fca83cbb12361f795d25464c92088692c1637bc6
Felix Ammann <ammann@puzzle.ch>
2017-03-21 10:01:13 +0100

    user authenticator test

--- test/controllers/authentication/user_authenticator_test.rb
+++ test/controllers/authentication/user_authenticator_test.rb
@@ -11,6 +11,58 @@ require 'test_helper'
 
 class UserAuthenticatorTest < ActiveSupport::TestCase
 
+  test 'authenticates bob' do
+    @params = {username: 'bob', password: 'password'}
+
+    assert_equal true, authenticate
+  end
+
+  test 'authentication invalid if blank password' do
+    @params = {username: 'bob', password: ''}
+
+    assert_equal false, authenticate
+  end
+
+  test 'authenticates against ldap' do
+    @params = {username: 'bob', password: 'ldappw'}
+    bob.update_attribute(:auth, 'ldap')
+    LdapTools.expects(:ldap_login).with('bob', 'ldappw').returns(true)
+    assert_equal true, authenticate
+  end
+
+  test 'doesnt authenticate against ldap' do
+    @params = {username: 'bob', password: 'wrongldappw'}
+    bob.update_attribute(:auth, 'ldap')
+    LdapTools.expects(:ldap_login).with('bob', 'wrongldappw').returns(false)
+
+    assert_equal false, authenticate
+  end
+
+  test 'increasing of failed login attempts and it\'s defined delays' do
+    @params = {username: 'bob', password: 'wrong password'}
+    bruteForceDetector = Authentication::BruteForceDetector.new(bob)
+    locktimes = Authentication::BruteForceDetector::LOCK_TIME_FAILED_LOGIN_ATTEMPT
+    assert_equal 10, locktimes.count
+    locktimes.each_with_index do |timer, i|
+      attempt = i + 1
+
+      bob.update_attribute(:failed_login_attempts, attempt)
+      last_failed_login_time = Time.now.utc - (locktimes[attempt].seconds)
+      bob.update_attribute(:last_failed_login_attempt_at, last_failed_login_time)
+
+      authenticate
+
+      assert_not bruteForceDetector.send(:user_temporarly_locked?), 'bob shouldnt be locked temporarly'
+
+      authenticate
+
+      return if attempt == locktimes.count - 1
+
+      assert_equal attempt + 1, bob.failed_login_attempts
+      assert last_failed_login_time.to_i <= bob.reload.last_failed_login_attempt_at.to_i
+    end
+  end
+
   test 'authentication fails if required params missing' do
     @params = {}
 
@@ -45,5 +97,9 @@ class UserAuthenticatorTest < ActiveSupport::TestCase
 
   def authenticator
     @authenticator ||= Authentication::UserAuthenticator.new(@params)
+  end
+
+  def bob
+    users(:bob)
   end
 end
